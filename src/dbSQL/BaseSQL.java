0c259f21742296dd4e17590ad0e9beaee5308dc7
MPankaj <MPankaj@LPT201-M3.quinnox.corp>
2016-04-04 17:34:39 +0530

    BaseSQL

--- src/dbSQL/BaseSQL.java
+++ src/dbSQL/BaseSQL.java
@@ -1,22 +1,91 @@
-package dbSQL;
+package src.dbSQL;
 
 import java.sql.Connection;
 import java.util.Collection;
+import java.util.Hashtable;
+import java.util.Vector;
 
+import util.ClassInstantiateUtil;
+import util.commonUTIL;
+
+import beans.Attribute;
+import beans.AttributeContainer;
 import beans.BaseBean;
 
-public interface BaseSQL {
+public abstract class BaseSQL {
           
-	  public BaseBean insertSQL(String sql,Connection con);
-	  public boolean updateSQL(String sql,Connection con);
-	  public boolean deleteSQL(String sql,Connection con);
+	  public abstract BaseBean insertSQL(String sql,Connection con);
+	  public abstract boolean updateSQL(String sql,Connection con);
+	  public abstract boolean deleteSQL(String sql,Connection con);
 
-	  public BaseBean insertSQL(BaseBean sql,Connection con);
-	  public boolean updateSQL(BaseBean sql,Connection con);
-	  public boolean deleteSQL(BaseBean sql,Connection con);
-	  public BaseBean select(int id,Connection con);
-	  public BaseBean select(String name,Connection con);
-	  public Collection selectWhere(String where,Connection con);
-	  public Collection selectALLData(Connection con);
+	  public abstract BaseBean insertSQL(BaseBean sql,Connection con);
+	  public abstract boolean updateSQL(BaseBean sql,Connection con);
+	  public abstract boolean deleteSQL(BaseBean sql,Connection con);
+	  public abstract BaseBean select(int id,Connection con);
+	  public abstract BaseBean select(String name,Connection con);
+	  public abstract Collection selectWhere(String where,Connection con);
+	  public abstract Collection selectALLData(Connection con);
+	  public abstract int count(String sql,Connection con);
+	 
+	  
+	  
+	  // this method will go when insert 
+	  public static void insertAttributes(BaseBean bean,int entityID,String entityType)  {
+		  AttributeContainer attributes = (AttributeContainer) bean;
+		 Vector<Attribute> attributeData = attributes.getAttributes();
+		 if(!commonUTIL.isEmpty(attributeData)) {
+			 for(int i=0;i<attributeData.size();i++) {
+				 Attribute att =  attributeData.get(i);
+				BaseSQL baseSQL = makeSQLObject(entityType);
+				baseSQL.insertSQL(att, dsSQL.getConn());
+			 }
+		 }
+	  }
+	  public AttributeContainer getAttributes(int entityID,String entityType)  {
+		  AttributeContainer attributeData = new AttributeContainer();
+		  Vector<Attribute> attributes = new Vector<Attribute>();
+		  String sql = " id = "+entityID+ " and ";
+		//  AttributeSQL.SelectWhere(entityID,)
+		  return attributeData;
+	  }
+	  public AttributeContainer deleteAttributes(int entityID,String entityType,String attributeName)  {
+		  AttributeContainer attributeData = new AttributeContainer();
+		  Vector<Attribute> attributes = new Vector<Attribute>();
+		  String sql = " id = "+entityID+ " and ";
+		//  AttributeSQL.SelectWhere(entityID,)
+		  return attributeData;
+	  }
+	  public AttributeContainer updateAttributes(int entityID,String entityType)  {
+		  AttributeContainer attributeData = new AttributeContainer();
+		  Vector<Attribute> attributes = new Vector<Attribute>();
+		  String sql = " id = "+entityID+ " and ";
+		//  AttributeSQL.SelectWhere(entityID,)
+		  return attributeData;
+	  }
+	  
+	  static public Hashtable<String, BaseSQL> sqlCache = new Hashtable<String, BaseSQL>();
+
+		private static BaseSQL makeSQLObject(String sqlName) {
+			BaseSQL sql = null;
+			 
+			if (!commonUTIL.isEmpty(sqlName)) {
+				String sqlObject = "dbSQL." + sqlName +"AttributeSQL";
+				try {
+					sql = sqlCache.get(sqlName);
+					if (sql == null) {
+						Class sqlC = ClassInstantiateUtil
+								.getClass(sqlObject, false);
+						if (sqlC != null) {
+							sql = (BaseSQL) sqlC.newInstance();
+							sqlCache.put(sqlName.trim(), sql);
+						}
+					}
+				} catch (InstantiationException | IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					commonUTIL.displayError("ReferenceDataImp", "makeSQLObject", e);
+				}
+			}
+			return sql;
+		}
 	  
 }
