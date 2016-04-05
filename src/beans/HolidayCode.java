1134094c7c4e1466a68cc96c05a316eb2a51e6c5
MPankaj <MPankaj@LPT201-M3.quinnox.corp>
2016-04-05 08:15:06 +0530

    Holiday Bean is replace with HolidayCode

---
+++ src/beans/HolidayCode.java
@@ -0,0 +1,48 @@
+package src.beans;
+
+import java.io.Serializable;
+
+public class HolidayCode implements Serializable {
+	
+	boolean isWeekEnd = false;
+	
+	
+	String currency;
+	int fweekday;
+	public int getFweekday() {
+		return fweekday;
+	}
+	public void setFweekday(int fweekday) {
+		this.fweekday = fweekday;
+	}
+	public int getSweekdday() {
+		return sweekdday;
+	}
+	public void setSweekdday(int sweekdday) {
+		this.sweekdday = sweekdday;
+	}
+	int sweekdday;
+	
+	
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getHdate() {
+		return hdate;
+	}
+	public void setHdate(String hdate) {
+		this.hdate = hdate;
+	}
+	String country;
+	String hdate; 
+
+}
