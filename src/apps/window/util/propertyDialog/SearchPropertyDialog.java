15292b9b40ae75f5399c21a47c99fbb80fe01d09
pankajmahadik1 <p.mahadik@unielements.com>
2016-04-15 05:32:13 +0530

    Dependancy error

--- src/apps/window/util/propertyDialog/SearchPropertyDialog.java
+++ src/apps/window/util/propertyDialog/SearchPropertyDialog.java
@@ -2,9 +2,13 @@ package apps.window.util.propertyDialog;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Comparator;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 
@@ -14,20 +18,38 @@ public class SearchPropertyDialog extends JDialog {
 	JPanel mainPanel = new JPanel();
 	 private final int WINDOW_WIDTH = 200;
 	    private final int WINDOW_DEPTH = 300;
-	  public  SearchPropertyWindowWrapper sw = null;
+
+		JButton okButton = new JButton("Ok");
+		JButton clearButton = new JButton("Clear");
+		JButton cancelButton = new JButton("Cancel");
+		JPanel southPanel = new JPanel();
+	  public  SearchPropertyWindowWrapper swrapper = null;
 	    private void createLayout() throws Exception {
 			   Container contentPane = getContentPane();
 			   contentPane.setSize(new java.awt.Dimension(WINDOW_WIDTH,WINDOW_DEPTH));
 			   mainPanel.setLayout(new BorderLayout());
-			    
+			  
+			   southPanel.setLayout(new FlowLayout());
+			   southPanel.add(okButton);
+			   southPanel.add(clearButton);
+			   southPanel.add(cancelButton);
+			   mainPanel.add(southPanel,BorderLayout.SOUTH);
+			   mainPanel.add(swrapper,BorderLayout.CENTER);
+			   cancelButton.addActionListener(new ActionListener() {
+		            public void actionPerformed(ActionEvent e) {
+		            	setVisible(false);
+		                dispose();
+		            }
+		        });
+			   
 			   
 			  
 			//   mainPanel.add(sw,BorderLayout.NORTH);
-			   contentPane.add( sw,BorderLayout.CENTER); 
+			   contentPane.add( mainPanel,BorderLayout.CENTER); 
 	    }
 	    protected void init(String searchType){
 		       try {
-		    	   sw =   new SearchPropertyWindowWrapper(searchType);
+		    	   swrapper =   new SearchPropertyWindowWrapper(searchType);
 					createLayout();
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
@@ -52,8 +74,11 @@ public class SearchPropertyDialog extends JDialog {
 			this(parent, modal, null, false, true); 
 			init(searchType);
 	    }
+	    public  JButton getCancelButton() { return cancelButton; }
+		   public  JButton getOkButton() { return okButton; }
+		   public JButton getClearButton() { return clearButton; }
 
-	    public JPanel getMainPanel() { return sw; }
+	    public JPanel getMainPanel() { return mainPanel; }
 	
 
 }
