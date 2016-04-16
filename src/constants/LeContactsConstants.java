45ec7fae3fee1a5311f5e16034a42cc22da908f2
pankajmahadik1 <p.mahadik@unielements.com>
2016-04-16 08:29:05 +0530

    Constants add for Book,CounteryParty,Product,CurrencyDefault,CurrencyPair

--- src/constants/LeContactsConstants.java
+++ src/constants/LeContactsConstants.java
@@ -3,12 +3,27 @@ package constants;
  public class LeContactsConstants { 
 
  final public static String WINDOW_NAME  = "Contact";
- final public static String LEID   = "LeID";
+ final public static String ID   = "ID";
+ final public static String ENTITYNAME   = "Entity Name";
+ final public static String ROLE   = "Role";
  final public static String CONTACTTYPE   = "ContactType";
- final public static String ADDRESS   = "Address";
- final public static int SPLITWINDOWLOCATION = 0;
- final public static int WINDOWWIDTH = 27;
- final public static int WINDOWHIGHT =80 ;
-public static final String SEARCHTEXTBOX = "ContactSearch";
-public static final String LOADALLLECONTACTS = "Load ContactDetails";
+ final public static String FIRSTNAME   = "FirstName";
+ final public static String LASTNAME   = "LastName";
+ final public static String MAILINGADDRESS1   = "MailingAddress1";
+ final public static String MAILINGADDRESS2   = "MailingAddress2";
+ final public static String COUNTRY   = "Country";
+ final public static String CITY   = "City";
+ final public static String STATE   = "State";
+ final public static String ZIPCODE   = "ZipCode";
+ final public static String SWIFTCODE   = "SwiftCode";
+ final public static String PHONE   = "Phone";
+ final public static String FAX   = "Fax";
+ final public static String EMAILID   = "EmailID";
+ final public static String MAPJAVAOBJECT   = "MapJavaObject";
+ final public static String PRODUCTTYPE   = "ProductType";
+ final public static int SPLITWINDOWLOCATION =370;
+ final public static int WINDOWWIDTH = 1227;
+ final public static int WINDOWHIGHT =480 ;
+public static final String SEARCHTEXTBOX = "LeContactsSearch";
+public static final String LOADALLLECONTACTS = " Load LeContactsDetails";
 }
