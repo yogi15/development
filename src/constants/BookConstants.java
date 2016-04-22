e3e489e9d8812e2c3691c306a8ce8c8e2195178b
pankajmahadik1 <p.mahadik@unielements.com>
2016-04-22 15:09:27 +0530

    BookConstant

--- src/constants/BookConstants.java
+++ src/constants/BookConstants.java
@@ -3,21 +3,18 @@ package constants;
  public class BookConstants { 
 
  final public static String WINDOW_NAME  = "Book";
- final public static String LOCATION   = "Location";
- final public static String BOOKNO   = "BookID";
+ final public static String ID   = "ID";
  final public static String BOOKNAME   = "BookName";
- final public static String BOOKATTRIBUTES   = "BookAttributes";
+ final public static String CUSTNAME   = "CustName";
+ final public static String COUNTRY   = "Country";
+ final public static String HOLIDAY   = "Holiday";
+ final public static String TIMEZONE   = "TimeZone";
  final public static String CURRENCY   = "Currency";
-
- final public static String LEGALENTITY   = "LegalEntity";
+ final public static String MAPJAVAOBJECT   = "MapJavaObject";
+ final public static String ATTRIBUTES   = "Attributes";
  final public static int SPLITWINDOWLOCATION =370;
  final public static int WINDOWWIDTH = 1227;
  final public static int WINDOWHIGHT =480 ;
 public static final String SEARCHTEXTBOX = "BookSearch";
-
-public static final String LOADBUTTON = "Load BookDetails";
-public static final String HOLIDAYCODE = "HolidayCode";
-
-public static final String TIMEZONE = "TimeZone";
-
+public static final String LOADALLBOOK = "Load BookDetails";
 }
