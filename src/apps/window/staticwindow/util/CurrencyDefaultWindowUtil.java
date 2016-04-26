1df80203e96cce56c99b0f9d616c5e9ff906c977
ManishSarang <manishs@unielements.com>
2016-04-26 15:40:02 +0530

    Manish

--- src/apps/window/staticwindow/util/CurrencyDefaultWindowUtil.java
+++ src/apps/window/staticwindow/util/CurrencyDefaultWindowUtil.java
@@ -1,195 +1,246 @@
-package apps.window.staticwindow.util; 
+package apps.window.staticwindow.util;
 
 
- import java.util.Vector;
-import apps.window.util.tableModelUtil.TableUtils;import util.cacheUtil.ReferenceDataCache;
+import java.util.Vector;
+import apps.window.util.tableModelUtil.TableUtils;
+import util.cacheUtil.ReferenceDataCache;
 import util.commonUTIL;
 import apps.window.staticwindow.BasePanel;
 import apps.window.staticwindow.CurrencyDefaultWindow;
-import beans.CurrencyDefault; 
+import beans.CurrencyDefault;
+import beans.CurrencyPair;
+import beans.LegalEntity;
 import beans.WindowSheet;
 import com.jidesoft.grid.Property;
 import constants.CommonConstants;
 import constants.CurrencyDefaultConstants;
+import constants.CurrencyPairConstants;
 import constants.BeanConstants;
+
 public class CurrencyDefaultWindowUtil extends BaseWindowUtil {
- CurrencyDefaultWindow currencydefaultWindow= null;
-CurrencyDefault currencydefault = null;
- String currencydefaultName;
-/**
- * @return the windowName
- */
-public String getWindowName() {
-	return CurrencyDefaultConstants.WINDOW_NAME;
-}
+	CurrencyDefaultWindow currencydefaultWindow = null;
+	CurrencyDefault currencydefault = null;
+	String currencydefaultName;
+
 	/**
- * @param windowName the windowName to set
- */
-public void setWindowName(String currencydefaultName) {
-	this.currencydefaultName = currencydefaultName;
-}
-/**
- * @return the currencydefault
- */
-	public CurrencyDefault getCurrencyDefault() {
-	return currencydefault;
-	}
-		/**
- * @param currencydefault the currencydefault to set
+	 * @return the windowName
 	 */
-public void setCurrencyDefault(CurrencyDefault  currencydefault) {
-	  this.currencydefault = currencydefault;
-}@Override
-public boolean validate( ) {
+	public String getWindowName() {
+		return CurrencyDefaultConstants.WINDOW_NAME;
+	}
+
+	/**
+	 * @param windowName
+	 *            the windowName to set
+	 */
+	public void setWindowName(String currencydefaultName) {
+		this.currencydefaultName = currencydefaultName;
+	}
+
+	/**
+	 * @return the currencydefault
+	 */
+	public CurrencyDefault getCurrencyDefault() {
+		return currencydefault;
+	}
+
+	/**
+	 * @param currencydefault
+	 *            the currencydefault to set
+	 */
+	public void setCurrencyDefault(CurrencyDefault currencydefault) {
+		this.currencydefault = currencydefault;
+	}
+
+	@Override
+	public boolean validate() {
 		// TODO Auto-generated method stub
 		boolean flag = false;
-			return validate(getCurrencyDefault(),CurrencyDefaultConstants.WINDOW_NAME);
-			}
+		return validate(getCurrencyDefault(), CurrencyDefaultConstants.WINDOW_NAME);
+	}
+
 	@Override
-					public Vector<String> fillData(String action) {
-				// TODO Auto-generated method stub
-			return null;
-}
+	public Vector<String> fillData(String action) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	@Override
-public void actionMapper(String action) {
-	// TODO Auto-generated method stub
-Property prop = currencydefaultWindow.propertyTable.getPropertyTable().getSelectedProperty(); 
-if(action.equalsIgnoreCase(CommonConstants.SAVEASNEWBUTTON)) {
-saveAsNewButtonAction();
-}
-if(action.equalsIgnoreCase(CommonConstants.NEWBUTTON)) {
-newButtonAction();
-	}
-		if(action.equalsIgnoreCase(CommonConstants.LOADBUTTON)) {
-loadButtonAction();
-	}
-	if(action.equalsIgnoreCase(CurrencyDefaultConstants.SEARCHTEXTBOX)) {
-searchTextAction();
-	}
-if(action.equalsIgnoreCase(CommonConstants.RIGHTSIDECENTERTABLE)) {
-	rightSideCenterTableAction();
-}
-	if(action.equalsIgnoreCase(CommonConstants.DELETEBUTTON)) {
-	deleteButtonAction();
-}
-if(action.equalsIgnoreCase(CommonConstants.SAVEBUTTON)) {
-saveButtonAction();
-}
-if (action.equalsIgnoreCase(CommonConstants.HIERARACHICALTABLE)) {
-hierarachicalTableAction();
- }
- if (action.equalsIgnoreCase(CurrencyDefaultConstants.LOADALLCURRENCYDEFAULT)) {
- 	loadButtonAction();
- }
-	}				
-@Override
-public void setWindow(BasePanel windowName) {
-	// TODO Auto-generated method stub
-	currencydefaultWindow = (CurrencyDefaultWindow)windowName;
-setCurrencyDefault(currencydefaultWindow.getCurrencyDefault()); 
-}
-private void hierarachicalTableAction() {
-	if (currencydefaultWindow.hierarchicalTable.getSelectedRow() != -1) {
-			currencydefaultWindow.propertyTable.setPropertiesValues(currencydefaultWindow.model.getRow(currencydefaultWindow.hierarchicalTable.getSelectedRow()));
-		currencydefaultWindow.setCurrencyDefault(currencydefaultWindow.model.getRow(currencydefaultWindow.hierarchicalTable.getSelectedRow()));
-		setCurrencyDefault(currencydefaultWindow.model.getRow(currencydefaultWindow.hierarchicalTable.getSelectedRow()));
+	public void actionMapper(String action) {
+		// TODO Auto-generated method stub
+		Property prop = currencydefaultWindow.propertyTable.getPropertyTable().getSelectedProperty();
+		
+		if (action.equalsIgnoreCase(CommonConstants.SAVEASNEWBUTTON)) {
+			saveAsNewButtonAction();
+		}
+		if (action.equalsIgnoreCase(CommonConstants.NEWBUTTON)) {
+			newButtonAction();
+		}
+		if (action.equalsIgnoreCase(CommonConstants.LOADBUTTON)) {
+			loadButtonAction();
+		}
+		if (action.equalsIgnoreCase(CurrencyDefaultConstants.SEARCHTEXTBOX)) {
+			searchTextAction();
+		}
+		if (action.equalsIgnoreCase(CommonConstants.RIGHTSIDECENTERTABLE)) {
+			rightSideCenterTableAction();
+		}
+		if (action.equalsIgnoreCase(CommonConstants.DELETEBUTTON)) {
+			deleteButtonAction();
+		}
+		
+		if (action.equalsIgnoreCase(CommonConstants.SAVEBUTTON)) {
+			saveButtonAction();
+		}
+		if (action.equalsIgnoreCase(CommonConstants.HIERARACHICALTABLE)) {
+			hierarachicalTableAction();
+		}
+		if (action.equalsIgnoreCase(CurrencyDefaultConstants.LOADALLCURRENCYDEFAULT)) {
+			loadButtonAction();
 		}
 	}
-private void saveButtonAction() {
-currencydefaultWindow.propertyTable
-.setfillValues(currencydefault);
-setCurrencyDefault((CurrencyDefault) currencydefaultWindow.propertyTable.getBean());
-//if(validate( )) 
-// if(ReferenceDataCache.updateCurrencyDefault(getCurrencyDefault())) {
-//if(currencydefaultWindow.rightSideCenterTable.getSelectedRow() != -1) {
-	 // int i=  TableUtils.getSelectedRowIndex( currencydefaultWindow.rightSideCenterTable);
-	  //currencydefaultWindow.model.udpateValueAt(getCurrencyDefault(), i, 0);
- //}
- //}
-}		
-private void saveAsNewButtonAction() { 
-CurrencyDefault currencydefault = new CurrencyDefault();
-	currencydefaultWindow.propertyTable.setfillValues(currencydefault);
-	 setCurrencyDefault(currencydefault);
-//if(validate( )){
-// currencydefault = ReferenceDataCache.saveCurrencyDefault(currencydefault); 
- // currencydefaultWindow.model.addRow(getCurrencyDefault());
- // setCurrencyDefault(currencydefault);
-  //}
-}
-private void newButtonAction() {
-currencydefaultWindow.propertyTable.clearPropertyValues();
-currencydefaultWindow.model.clear();
-setCurrencyDefault(null);
+
+	@Override
+	public void setWindow(BasePanel windowName) {
+		// TODO Auto-generated method stub
+		currencydefaultWindow = (CurrencyDefaultWindow) windowName;
+		setCurrencyDefault(currencydefaultWindow.getCurrencyDefault());
 	}
-private void loadButtonAction() {
-	newButtonAction();
-String searchText =   currencydefaultWindow.currencydefaultSearchTextField.getText();
- if(!commonUTIL.isEmpty(searchText)) {
-Vector<CurrencyDefault> data = null;//ReferenceDataCache.selectCurrencyDefaults(searchText);
-currencydefaultWindow.model.clear();
-if(!commonUTIL.isEmpty(data)) {
-CurrencyDefault firstRecord = data.get(0);
-for(int i=0;i<data.size();i++) {
- currencydefaultWindow.model.addRow((CurrencyDefault)data.get(i));
-				}
-				currencydefaultWindow.propertyTable.setPropertiesValues(firstRecord);
-				 setCurrencyDefault(firstRecord);
-		} 
-}else {
-		Vector<CurrencyDefault> data = (Vector<CurrencyDefault>) ReferenceDataCache.selectALLData(BeanConstants.CURRENCYDEFAULT);
-		if (!commonUTIL.isEmpty(data)) {
-currencydefaultWindow.model.clear();
-			CurrencyDefault firstRecord = data.get(0);
-			for (int i = 0; i < data.size(); i++) {
-					currencydefaultWindow.model.addRow((CurrencyDefault) data.get(i));
-				}
-				currencydefaultWindow.propertyTable.setPropertiesValues(firstRecord);
-		setCurrencyDefault(firstRecord);
-			}
-	 }
-}
-public void loadData(int id) {
+
+	private void hierarachicalTableAction() {
+		if (currencydefaultWindow.hierarchicalTable.getSelectedRow() != -1) {
+			currencydefaultWindow.propertyTable.setPropertiesValues(
+					currencydefaultWindow.model.getRow(currencydefaultWindow.hierarchicalTable.getSelectedRow()));
+			currencydefaultWindow.setCurrencyDefault(
+					currencydefaultWindow.model.getRow(currencydefaultWindow.hierarchicalTable.getSelectedRow()));
+			setCurrencyDefault(
+					currencydefaultWindow.model.getRow(currencydefaultWindow.hierarchicalTable.getSelectedRow()));
+		}
+	}
+
+	private void saveButtonAction() {
+		currencydefaultWindow.propertyTable.setfillValues(currencydefault);
+		setCurrencyDefault((CurrencyDefault)currencydefaultWindow.propertyTable.getBean());
+		// if(validate( ))
+		 if(ReferenceDataCache.updateSQL(getCurrencyDefault(),CurrencyDefaultConstants.WINDOW_NAME)) {
+		 if(currencydefaultWindow.rightSideCenterTable.getSelectedRow() != -1)
+		 {
+		 int i= TableUtils.getSelectedRowIndex(
+		 currencydefaultWindow.rightSideCenterTable);
+		 currencydefaultWindow.model.udpateValueAt(getCurrencyDefault(), i, 0);
+		 }
+		 }
+	}
+	private void saveAsNewButtonAction() {
+		CurrencyDefault currencydefault = new CurrencyDefault();
+		System.out.println(currencydefault.toString());
+		currencydefaultWindow.propertyTable.setfillValues(currencydefault);
+		setCurrencyDefault(currencydefault);
+		// if(validate( )){
+		 if(ReferenceDataCache.insertSQL(getCurrencyDefault(),CurrencyDefaultConstants.WINDOW_NAME) != null) {
+			 if(currencydefaultWindow.rightSideCenterTable.getSelectedRow() != -1)
+		 currencydefaultWindow.model.addRow(getCurrencyDefault());
+		 setCurrencyDefault(currencydefault);
+		 }	 }
+	
+
+	/*private void saveAsNewButtonAction() {
+		CurrencyDefault currencydefault = new CurrencyDefault();
+		currencydefaultWindow.propertyTable.setfillValues(currencydefault);
+		setCurrencyDefault(currencydefault);
+		// if(validate( )){
+		currencydefault = (CurrencyDefault) ReferenceDataCache.insertSQL(getCurrencyDefault(), currencydefaultName);
+		currencydefaultWindow.model.addRow(getCurrencyDefault());
+		 setCurrencyDefault(currencydefault);
+		 }
+		 */
+		 
+	
+
+	private void newButtonAction() {
+		currencydefaultWindow.propertyTable.clearPropertyValues();
+		currencydefaultWindow.model.clear();
+		setCurrencyDefault(null);
+	}
+
+	private void loadButtonAction() {
 		newButtonAction();
-		Vector<CurrencyDefault> data = null;// ReferenceDataCache.getCurrencyDefault(id);
+		String searchText = currencydefaultWindow.currencydefaultSearchTextField.getText();
+		if (commonUTIL.isEmpty(searchText)) {
+			Vector<CurrencyDefault> data = (Vector<CurrencyDefault>) ReferenceDataCache.selectALLData(BeanConstants.CURRENCYDEFAULT);
+			
 			currencydefaultWindow.model.clear();
 			if (!commonUTIL.isEmpty(data)) {
 				CurrencyDefault firstRecord = data.get(0);
 				for (int i = 0; i < data.size(); i++) {
-			currencydefaultWindow.model.addRow((CurrencyDefault) data.get(i));
+					currencydefaultWindow.model.addRow((CurrencyDefault) data.get(i));
 				}
 				currencydefaultWindow.propertyTable.setPropertiesValues(firstRecord);
 				setCurrencyDefault(firstRecord);
+			}
+		} else {
+			Vector<CurrencyDefault> data = (Vector<CurrencyDefault>) ReferenceDataCache.selectALLData(BeanConstants.CURRENCYDEFAULT);
+			if (!commonUTIL.isEmpty(data)) {
+				currencydefaultWindow.model.clear();
+				CurrencyDefault firstRecord = data.get(0);
+				for (int i = 0; i < data.size(); i++) {
+					currencydefaultWindow.model.addRow((CurrencyDefault) data.get(i));
+				}
+				currencydefaultWindow.propertyTable.setPropertiesValues(firstRecord);
+				setCurrencyDefault(firstRecord);
+			}
+		}
 	}
-	}
- private void rightSideCenterTableAction() {
-	if(currencydefaultWindow.rightSideCenterTable.getSelectedRow() != -1) 
-		 currencydefaultWindow.propertyTable.setPropertiesValues( currencydefaultWindow.model.getRow(currencydefaultWindow.rightSideCenterTable.getSelectedRow()));
-	currencydefaultWindow.setCurrencyDefault(currencydefaultWindow.model.getRow(currencydefaultWindow.rightSideCenterTable .getSelectedRow()));
-	setCurrencyDefault(currencydefaultWindow.model .getRow(currencydefaultWindow.rightSideCenterTable .getSelectedRow()));
-}
-private void searchTextAction() {
-	loadButtonAction();
-}
-// check Null pointerException.
-private void deleteButtonAction() {
-	try {
-//if(ReferenceDataCache.deleteCurrencyDefault(currencydefault)) {
-//if( currencydefaultWindow.rightSideCenterTable.getSelectedRow() != -1) {
-	//currencydefaultWindow.model.delRow(currencydefaultWindow.rightSideCenterTable.getSelectedRow()); 
-	//}
-//setCurrencyDefault(null);
- //currencydefaultWindow.propertyTable.clearPropertyValues();
-//}	
-} catch(Exception e) {
-commonUTIL.displayError(CurrencyDefaultConstants.WINDOW_NAME+"Util" , "deleteButtonAction", e);
-}
-}
-@Override
-public void windowstartUpData() {
- 
 
-}@Override
-public void clearALL() {
-}
+	public void loadData(int id) {
+		newButtonAction();
+		Vector<CurrencyDefault> data = null;// ReferenceDataCache.getCurrencyDefault(id);
+		currencydefaultWindow.model.clear();
+		if (!commonUTIL.isEmpty(data)) {
+			CurrencyDefault firstRecord = data.get(0);
+			for (int i = 0; i < data.size(); i++) {
+				currencydefaultWindow.model.addRow((CurrencyDefault) data.get(i));
+			}
+			currencydefaultWindow.propertyTable.setPropertiesValues(firstRecord);
+			setCurrencyDefault(firstRecord);
+		}
+	}
+
+	private void rightSideCenterTableAction() {
+		if (currencydefaultWindow.rightSideCenterTable.getSelectedRow() != -1)
+			currencydefaultWindow.propertyTable.setPropertiesValues(
+					currencydefaultWindow.model.getRow(currencydefaultWindow.rightSideCenterTable.getSelectedRow()));
+		currencydefaultWindow.setCurrencyDefault(
+				currencydefaultWindow.model.getRow(currencydefaultWindow.rightSideCenterTable.getSelectedRow()));
+		setCurrencyDefault(
+				currencydefaultWindow.model.getRow(currencydefaultWindow.rightSideCenterTable.getSelectedRow()));
+	}
+
+	private void searchTextAction() {
+		loadButtonAction();
+	}
+
+	// check Null pointerException.
+	private void deleteButtonAction() {
+		try {
+			 if(ReferenceDataCache.deleteSQL(getCurrencyDefault(),CurrencyDefaultConstants.WINDOW_NAME)) {
+				 if(currencydefaultWindow.rightSideCenterTable.getSelectedRow() != -1) {
+		 currencydefaultWindow.model.delRow(currencydefaultWindow.rightSideCenterTable.getSelectedRow());
+			 }
+			setCurrencyDefault(null);
+			currencydefaultWindow.propertyTable.clearPropertyValues();
+			}
+		} catch (Exception e) {
+			commonUTIL.displayError(CurrencyDefaultConstants.WINDOW_NAME + "Util", "deleteButtonAction", e);
+		}
+	}
+
+	@Override
+	public void windowstartUpData() {
+
+	}
+
+	@Override
+	public void clearALL() {
+	}
 }
